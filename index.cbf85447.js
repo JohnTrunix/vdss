7e77bab1cf384745f75b7eaddfccd6f1c7b1bc50
JohnTrunix <61423865+JohnTrunix@users.noreply.github.com>
2023-05-11 22:49:33 +0000

    Deploying to gh-pages from @ JohnTrunix/vdss@7a87779405a6a437cb521942f1d985d397cc6094 🚀

--- index.cbf85447.js
+++ index.cbf85447.js
@@ -1,2 +1,2 @@
 function changeLanguage(){var e=document.getElementById("language-select"),n=e.options[e.selectedIndex].value;"en"!=n&&(window.alert(`Sorry, [ ${n} ] is currently not implemented. \nWe are working on other language implementations!`),e.value="en")}
-//# sourceMappingURL=index.ce835e68.js.map
+//# sourceMappingURL=index.cbf85447.js.map
