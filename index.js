d790eec3c36e1900f63a110006c4f71fe8864d02
JohnTrunix <john@trunix.ch>
2023-05-30 12:12:29 +0200

    style tweaks

--- index.js
+++ index.js
@@ -28,8 +28,8 @@ new fullpage("#fullpage", {
     slidesNavigation: true,
     slidesNavPosition: "bottom",
     controlArrowsHTML: [
-        '<div class="my-arrow left">◄</div>',
-        '<div class="my-arrow right">►</div>',
+        '<svg xmlns="http://www.w3.org/2000/svg"  width="48" height="48" viewBox="0 0 24 24" stroke-width="1.75" stroke="#fff" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M15 6l-6 6l6 6"></path></svg>',
+        '<svg xmlns="http://www.w3.org/2000/svg"  width="48" height="48" viewBox="0 0 24 24" stroke-width="1.75" stroke="#fff" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M9 6l6 6l-6 6"></path></svg>',
     ],
 
     anchors: [
